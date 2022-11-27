5f29ae9868178cba5d8f5020eed4bd9731aa263c
jy <wruomma@naver.com>
2022-11-27 13:00:33 +0900

    Method_input dataInput3(Method_Input ip) Variable

--- src/TEST/Method_Input.java
+++ src/TEST/Method_Input.java
@@ -21,7 +21,7 @@ public class Method_Input {
 
 	public void dataInput(Method_Input i) {
 		i.name2 = "Dev.Malang2"; i.age2 = 292; i.addr2 = "대구2";
-		System.out.println("이름: " + name2 + " 나이: " + age2 + " 지역: " + addr2);}
+		System.out.println("이름: " + i.name2 + " 나이: " + i.age2 + " 지역: " + i.addr2);}
 	
 	public void dataInput2() {
 		// 생성자의 this를 호출한다.
@@ -42,15 +42,15 @@ public class Method_Input {
 		 * 
 		 * 반대로, toString이 없거나 주석처리되어 있는 경우에는 해당 변수의 주소값이 출력된다.
 		 */
-		System.out.println(new Method_Input(name, age, addr));
+//		System.out.println(new Method_Input(name, age, addr));
 		
 		// 생성자에 직접 값을 입력한다.
-		System.out.println(new Method_Input("Dev.Malang", 29, "대구"));
+//		System.out.println(new Method_Input("Dev.Malang", 29, "대구"));
 		
 		// ip 참조변수에 있는(public class Input) dataInput를 호출한다.
 		ip.dataInput(ip);
 		
 		// dataInput2 메소드에서 dataInput 메소드를 호출한다.
-		ip.dataInput2();
+//		ip.dataInput2();
 	}
 }
